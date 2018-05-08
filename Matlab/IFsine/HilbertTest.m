6e708526a1316bf1e7160e17df65671aeed455a3
lsangild <lsangild@gmail.com>
2018-05-08 12:10:52 +0200

    Manual version of Instantaneous Frequency implemented in MATLAB

--- Matlab/IFsine/HilbertTest.m
+++ Matlab/IFsine/HilbertTest.m
@@ -22,45 +22,41 @@ y=[y(1,:);
    zeros(N/2-1, W)];
 z=ifft(y);
 
-% instfreq
-
-
+%% Do easy way
 instfreq = Fs/(2*pi)*diff(unwrap(angle(z)));
 za = angle(z);
 zau = unwrap(angle(z));
 zaud = diff(unwrap(angle(z)));
 
-% Angle
+%% Angle
 z = atan2(imag(z),real(z));
 
-% Unwrap
-k = 1;
+%% Unwrap
 for i = 2:N
-  d = z(i) - z(i - 1);
-  if (d > pi)
-    d = d - 2 * pi * k;
-    k = k + 1;
-  elseif (d < -pi)
-    d = d + 2 * pi * k;
-    k = k + 1;
-  else
-    d = d;
+  dif = rem(z(i - 1) - z(i) + pi, 2* pi);
+  if (dif < 0)
+    dif = dif + 2 * pi;
   endif
-  z(i) = z(i - 1) + d;
+  z(i) = dif - pi;
+  z(i) = z(i - 1) - z(i);
 endfor
 
-% Diff
+%% Diff
 for k = 2:(numel(z))
-  z2(k - 1, 1) = z(k) - z(k - 1);
+  z(k - 1, 1) = z(k) - z(k - 1);
 endfor
+% Remove last diff
+z(end) = [];
 
-z2 = Fs/(2*pi)*z2;
+z = Fs/(2*pi)*z;
 
 %% Plot
-%plot(t(2:end),z2, '-r')
+plot(t(2:end),z, '-r')
 %hold on
-plot(t(2:end),instfreq)
+%plot(t(2:end),instfreq)
 xlabel('Time')
 ylabel('Hz')
 grid on
 title('Instantaneous Frequency')
+
+disp(mean(z(100:end-100)))
