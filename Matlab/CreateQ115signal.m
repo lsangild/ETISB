86c8c832d926e870f12a0920581e68b477fb0743
wolfeyem <mads.c.rosendahl@gmail.com>
2018-05-15 14:29:00 +0200

    kode der virker med 470Hz

--- Matlab/CreateQ115signal.m
+++ Matlab/CreateQ115signal.m
@@ -5,11 +5,11 @@ T = 0.04267/4;
 t=0:ts:T - 1/fs;
 
 scale = Amp;
-y=scale.*sin(2*pi*800*t)';
+y=scale.*sin(2*pi*888*t)';
 
 % gem i fil
 y16 = y*2^15;
-fid = fopen('x_signal470.txt', 'w');
+fid = fopen('x_signal888.txt', 'w');
 for i=1:length(y16)
     xtext = num2str(round(y16(i)));
     fprintf(fid, '%s,\r\n', xtext);
