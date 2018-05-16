ba093ff8cfe4d6db7a4d35937af6674459e2c171
lsangild <lsangild@gmail.com>
2018-05-16 11:39:04 +0200

    Changed HilbertTest to take chunks

--- Matlab/IFsine/HilbertTest.m
+++ Matlab/IFsine/HilbertTest.m
@@ -1,23 +1,28 @@
 %% Load data in stead
 Fs = 48000;
-y = csvread('x_signal470.txt');
+y = csvread('x_signal222.txt');
 y(:,2) = []; % Remove extra column
+blocksize = 128;
 
-ts = 1/Fs;
-T = length(y)*ts;
-t = 0:ts:(T - ts);
+% Check if blocksize is valid
+assert(ismember(blocksize, [2^7, 2^8, 2^9, 2^10, 2^11]) && blocksize <= length(y));
 
-%% Home made
-scale = 2^15;
-z = getIF(y, Fs, scale);
+% Create window
+h = hamming(blocksize);
 
-%% Plot
-plot(t(2:end),z, '-r')
-%hold on
-%plot(t(2:end),instfreq)
-xlabel('Time')
-ylabel('Hz')
-grid on
-title('Instantaneous Frequency')
+for i = 0:(length(y)/blocksize - 1)
+  y_block = y((i * blocksize + 1):((i + 1) * blocksize));
 
-disp((mean(z(100:end-100))/(scale))*pi)
+  ts = 1/Fs;
+  T = length(y_block)*ts;
+  t = 0:ts:(T - ts);
+
+  % Apply window
+  y_block = y_block .* h;
+
+  %% Home made
+  scale = 2^15;
+  z = getIF(y_block, Fs, scale);
+
+  disp((mean(z(floor(length(z)/3):end-ceil(length(z)/3)))/(scale))*pi)
+end
